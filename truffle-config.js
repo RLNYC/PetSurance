11677e234ef718f96225428a2bb214cb6b74ed9d
You Song Hou <yousonghou@yahoo.com>
2021-05-22 16:39:55 -0400

    add config to deploy contracts on Polygon Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,5 @@
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "".toString().trim();
 
 module.exports = {
   networks: {
@@ -6,6 +7,14 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    // truffle migrate --network matic
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
   },
   contracts_build_directory: './src/abis/',
